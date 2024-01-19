7a20c7d788d2c888fe39ec2ad1b35d15b7e57b96
Digvijay Singh <106017750+digvijaysinghh@users.noreply.github.com>
2024-01-19 16:34:30 +0530

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,6 +1,3 @@
-#Code By-Shivansh Vasu
-
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -30,6 +27,3 @@ rfc.fit(X_train, y_train)
 
 pickle.dump(rfc,open('model.pkl','wb'))
 
-
-
-#Code By-Shivansh Vasu
